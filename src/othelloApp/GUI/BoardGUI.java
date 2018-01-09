6d85cb57f59fadf245fff8d9be028b2371f345e8
LioraZ <liorazr@gmail.com>
2018-01-09 02:26:39 +0200

    trying to make game start

--- src/othelloApp/GUI/BoardGUI.java
+++ src/othelloApp/GUI/BoardGUI.java
@@ -1,10 +1,17 @@
 package othelloApp.GUI;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import othelloGame.Board;
+import othelloGame.Move;
+import othelloGame.Point;
 import othelloGame.Tile;
 
 
@@ -13,16 +20,30 @@ import java.io.IOException;
 
 public class BoardGUI extends GridPane {
     private Board board;
+   // private Parent scene;
 
     public BoardGUI(Board board) {
         this.board = board;
+        reload();
+       // this.scene = null;
+        /*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../board.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }*/
+    }
+
+    public void reload() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../board.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
-
-
         try {
-            fxmlLoader.load();
+            Parent scene = fxmlLoader.load();
+            //scene.getScene().getWindow();
+           // ((Stage)this.getScene().getWindow()).setScene(new Scene(scene, 400, 400));
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
@@ -65,5 +86,34 @@ public class BoardGUI extends GridPane {
     }
 
     public Board getBoard() {return this.board;}
+
+    public class MouseHandler implements EventHandler<MouseEvent> {
+        private double x, y;
+        @Override
+        public void handle(MouseEvent event) {
+            x = event.getX();
+            y = event.getY();
+            //System.out.println("mouse click detected!");
+            event.consume();
+        }
+        public Move returnMove() {
+            return processMousePressEvent(x, y);
+        }
+    }
+
+    public Move mousePressEvent() {
+        MouseHandler mouseHandler = new MouseHandler();
+        //mouseHandler.handle();
+        setOnMousePressed(mouseHandler);
+        return mouseHandler.returnMove();
+    }
+
+
+    public Move processMousePressEvent(double x, double y) {
+        int boardSize = (int) this.getPrefHeight();
+        int cellSize  = boardSize / board.getSize();
+        if (x < 0 || y < 0 || x >= boardSize || y >= boardSize) return new Move(new Point());
+        return new Move(new Point((int)x/cellSize, (int)y/cellSize));
+    }
 
 }
--- src/othelloApp/OthelloController.java
+++ src/othelloApp/OthelloController.java
@@ -2,13 +2,19 @@ package othelloApp;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import othelloApp.GUI.BoardGUI;
 import othelloApp.GUI.GameGUI;
 import othelloApp.GUI.GraphicUI;
 import othelloGame.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,33 +24,57 @@ import static java.lang.System.exit;
 
 public class OthelloController implements Initializable {
     private BoardGUI board;
+    private GraphicUI gui;
     @FXML
     private HBox root;
+
+    @FXML
+    private Button startScene;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.board = new BoardGUI(new Board(8));
+        this.gui = new GraphicUI(board);
         board.setPrefWidth(400);
         board.setPrefHeight(400);
         root.getChildren().add(0, board);
-        board.draw();
+        //board.draw();
 
         root.widthProperty().addListener((observable, oldValue, newValue) -> {
             double boardNewWidth = newValue.doubleValue() - 120;
             board.setPrefWidth(boardNewWidth);
-            board.draw();
+            //board.draw();
         });
 
         root.heightProperty().addListener((observable, oldValue, newValue) -> {
             board.setPrefHeight(newValue.doubleValue());
-            board.draw();
+            //board.draw();
         });
     }
 
     @FXML
     protected void startGame() {
+        Player player1 = new HumanPlayer(Tile.X);
+        Player player2 = new HumanPlayer(Tile.O);
+        Game game = new Game(player1, player2, gui, 8);
+        //board.reload();
+        game.run();
+        /*Stage curr = ((Stage)startScene.getScene().getWindow());
+        board.draw();
+        curr.show();*/
 
-        GameLogic logic = new RegularGameLogic();
+        /*try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("board.fxml"));
+            loader.setRoot(board);
+            //loader.setController(board);
+            Parent parent = loader.load();
+            board.draw();
+            ((Stage)startScene.getScene().getWindow()).setScene(new Scene(parent, 400, 400));
+        } catch (IOException eox) {
+            eox.printStackTrace();
+        }*/
+       // game.run();
+        /*GameLogic logic = new RegularGameLogic();
         Board board1 = new Board(8);
         boolean turn = true;
         GameStatus gameStatus = GameStatus.IN_PROGRESS;
@@ -52,15 +82,14 @@ public class OthelloController implements Initializable {
 
         /*if(turn) {
             player1TurnStatus = logic.turn(player1, board1, print);
-        } else player2TurnStatus = logic.turn(player2, board1, print);*/
+        } else player2TurnStatus = logic.turn(player2, board1, print);
 
         GameLogic gl = new RegularGameLogic();
         List<Move> movesList = gl.getMovesList(Tile.O, board.getBoard());
         GraphicUI graphicUI = new GraphicUI(board);
-        graphicUI.printMoves('O', movesList);
-
-
+        graphicUI.printMoves('O', movesList);*/
     }
+
     @FXML
     protected void endGame() {
         exit(1);
