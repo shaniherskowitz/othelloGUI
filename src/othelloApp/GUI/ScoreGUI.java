102b8a853f44a0ef12a2640e836f052a95aad290
Shani Herskowitz <shanizoe@gmail.com>
2018-01-12 01:18:49 +0200

    added color swatchs next to players

--- src/othelloApp/GUI/ScoreGUI.java
+++ src/othelloApp/GUI/ScoreGUI.java
@@ -1,7 +1,9 @@
 package othelloApp.GUI;
 
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import othelloGame.Board;
 
@@ -18,19 +20,29 @@ public class ScoreGUI extends GridPane {
     }
 
     public void draw(Board board, boolean turn) {
+        this.setVgap(10);
+        this.setPadding(new Insets(10, 10, 10, 6));
         this.getChildren().clear();
-        int current;
-        if (turn) current = 1;
-        else current = 2;
-        Text whosTurn = new Text("Players Turn: " + current);
-        Text player1 = new Text("1 Score: " + board.getXTiles());
-        Text player2 = new Text("2 Score: " + board.getOTiles());
+        Circle circle1 = new Circle(0, 0 , 7, player1Color);
+        Circle circle2 = new Circle(0, 0 , 7, player2Color);
+        Color current;
+        if (turn) current = player1Color;
+        else current = player2Color;
+        Circle circleTurn = new Circle(0, 0 , 12, current);
+        Text whosTurn = new Text("   Turn");
+        Text player1 = new Text("  Score: " + board.getXTiles());
+
+        Text player2 = new Text("  Score: " + board.getOTiles());
         player1.setStyle("-fx-font: 16 Courier;");
         player2.setStyle("-fx-font: 16 Courier;");
-        whosTurn.setStyle("-fx-font: 12 Courier;");
+        whosTurn.setStyle("-fx-font: 20 Courier;");
+
         this.add(whosTurn, 0, 5);
+        this.add(circleTurn,0,5);
+        this.add(circle1,0, 7);
         this.add(player1, 0, 7);
-        this.add(player2, 0, 9);
+        this.add(circle2,0, 10);
+        this.add(player2, 0, 10);
     }
 
     public void declareWinner(Board board) {
